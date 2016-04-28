1f4a3708e9192321fa4694ec21529c2260f6fab5
Wonderfall <wonderfall@schrodinger.io>
2016-04-28 20:21:44 +0200

    reverse: su-exec instead of gosu

--- reverse/run.sh
+++ reverse/run.sh
@@ -2,4 +2,4 @@
 touch /var/run/nginx.pid
 chown -R $UID:$GID /etc/nginx /var/log/nginx /var/run/nginx.pid /sites-enabled /conf.d /certs
 chmod -R 700 /certs
-gosu $UID:$GID nginx
+su-exec $UID:$GID nginx
