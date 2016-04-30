8de10a4c17ad0d778e7af062f9cbe2b912557b88
Wonderfall <wonderfall@schrodinger.io>
2016-04-30 12:57:18 +0200

    owncloud: move base to alpine

--- owncloud/run.sh
+++ owncloud/run.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-deluser www-data
-addgroup --gid $GROUPID www-data
-adduser --system --no-create-home --ingroup www-data --uid $USERID www-data
-crontab -u www-data /etc/cron.conf
+#!/bin/sh
+addgroup -g ${GID} owncloud && adduser -h /owncloud -s /bin/sh -D -G owncloud -u ${UID} owncloud
 
 if [ -f /owncloud/config/config.php ] && [ ! -f /config/config.php ]; then
   cp /owncloud/config/config.php /config/config.php
@@ -14,7 +11,8 @@ elif [ -f /config/config.php ]; then
   cp /config/config.php /owncloud/config/config.php
 fi
 
-chown -R www-data:www-data /owncloud /data /config /apps2
+touch /var/run/php-fpm.sock
+chown -R owncloud:owncloud /owncloud /data /config /apps2 /var/run/php-fpm.sock /var/lib/nginx /tmp
 ln -s /apps2 /owncloud
 
-supervisord -c /etc/supervisord.conf
+supervisord -c /etc/supervisor/supervisord.conf
