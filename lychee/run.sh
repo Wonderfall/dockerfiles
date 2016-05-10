22c28b98c21d0601966445206bf6af35e6d22b33
Wonderfall <wonderfall@schrodinger.io>
2016-05-11 00:08:40 +0200

    lychee: 3.1.0 -> 3.1.1

--- lychee/run.sh
+++ lychee/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 addgroup -g ${GID} lychee && adduser -h /lychee -s /bin/sh -D -G lychee -u ${UID} lychee
+mkdir /lychee/uploads/big /lychee/uploads/import /lychee/uploads/medium /lychee/uploads/thumb
 chown -R lychee:lychee /lychee /var/run/php-fpm.sock /var/lib/nginx /tmp
 supervisord -c /usr/local/etc/supervisord.conf
