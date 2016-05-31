b7572650ca59ad79fbad2f84c009151ec488f026
Wonderfall <wonderfall@schrodinger.io>
2016-05-31 18:22:11 +0200

    fix permissions errors

--- boring-nginx/run.sh
+++ boring-nginx/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 touch /var/run/nginx.pid
-chown -R $UID:$GID /etc/nginx /var/log/nginx /var/run/nginx.pid /sites-enabled /conf.d /certs /www
+chown -R $UID:$GID /etc/nginx /var/log/nginx /var/run/nginx.pid /sites-enabled /conf.d /certs /www /tmp
 chmod -R 700 /certs
 su-exec $UID:$GID nginx
