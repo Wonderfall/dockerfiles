92eae2739b513aed1fd8ec96f0e6355d938a82fe
kvaps <kvapss@gmail.com>
2016-08-16 20:02:21 +0300

    add: set default data directory

--- nextcloud/daily/run.sh
+++ nextcloud/daily/run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 addgroup -g ${GID} nextcloud && adduser -h /nextcloud -s /bin/sh -D -G nextcloud -u ${UID} nextcloud
 
+# Set default data directory
+if [ ! -f /config/config.php ]; then
+    echo -e "<?php\n\$CONFIG = array (\n  'datadirectory' => '/data',\n);" > /config/config.php
+fi
+
 touch /var/run/php-fpm.sock
 mkdir /tmp/fastcgi /tmp/client_body
 chown -R nextcloud:nextcloud /nextcloud /data /config /apps2 /var/run/php-fpm.sock /var/lib/nginx /tmp
