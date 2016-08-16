d0dae7fd8e14ce82f5c67f5f9646fb2e6950ed4c
kvaps <kvapss@gmail.com>
2016-08-16 19:23:31 +0300

    del: check for distributed config.php

--- nextcloud/daily/run.sh
+++ nextcloud/daily/run.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 addgroup -g ${GID} nextcloud && adduser -h /nextcloud -s /bin/sh -D -G nextcloud -u ${UID} nextcloud
 
-if [ -f /nextcloud/config/config.php ] && [ ! -f /config/config.php ]; then
-  mv /nextcloud/config/config.php /config/config.php
-fi
-ln -s /config/config.php /nextcloud/config/config.php
-
 touch /var/run/php-fpm.sock
 mkdir /tmp/fastcgi /tmp/client_body
 chown -R nextcloud:nextcloud /nextcloud /data /config /apps2 /var/run/php-fpm.sock /var/lib/nginx /tmp
+ln -s /config/config.php /nextcloud/config/config.php
 ln -s /apps2 /nextcloud
 
 supervisord -c /etc/supervisor/supervisord.conf
