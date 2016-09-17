ef5aa3147c4673c78d2116d13707bcdfd70fa9c2
Wonderfall <wonderfall@schrodinger.io>
2016-09-17 15:56:05 +0200

    remove tini if s6 is used

--- lychee/run.sh
+++ lychee/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /lychee/uploads/big /lychee/uploads/import /lychee/uploads/medium /lychee/uploads/thumb
 chown -R $UID:$GID /lychee /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
-exec su-exec $UID:$GID /sbin/tini -- /bin/s6-svscan /etc/s6.d
+exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
