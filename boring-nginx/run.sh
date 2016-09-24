8e5cea8be5bc13ce2863704fc829eb6fe027a8b1
Wonderfall <wonderfall@schrodinger.io>
2016-09-24 11:13:01 +0200

    nginx: fix #40

--- boring-nginx/run.sh
+++ boring-nginx/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+touch /var/run/nginx.pid
 chown -R $UID:$GID /etc/nginx /var/log/nginx /var/run/nginx.pid /sites-enabled /conf.d /certs /www /tmp
 chmod -R 700 /certs
 exec su-exec $UID:$GID /sbin/tini -- nginx
