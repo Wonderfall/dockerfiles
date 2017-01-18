68ab88e2a8f1b6e7aea938e67f46dc1a64ca61b0
Wonderfall <wonderfall@schrodinger.io>
2017-01-18 15:13:25 +0100

    piwik: fix chown

--- piwik/run.sh
+++ piwik/run.sh
@@ -5,5 +5,5 @@ if [ ! -f /config/config.ini.php ]; then
 fi
 
 ln -s /config/config.ini.php /piwik/config/config.ini.php
-chown -R $UID:$GID /piwik /config /var/log /etc/nginx /etc/php7 /var/lib/nginx /tmp /etc/s6.d
+chown -R $UID:$GID /piwik/config /piwik/tmp /config /var/log /etc/nginx /etc/php7 /var/lib/nginx /tmp /etc/s6.d
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
