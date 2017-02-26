3fbd11fdf00dfd3afdc56a1304629d87c670c389
Wonderfall <wonderfall@schrodinger.io>
2017-02-26 03:07:30 +0100

    update lychee && privatebin to php7.1

--- lychee/run.sh
+++ lychee/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /lychee/uploads/big /lychee/uploads/import /lychee/uploads/medium /lychee/uploads/thumb
-chown -R $UID:$GID /lychee /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
+chown -R $UID:$GID /lychee /etc/nginx /etc/php7.1 /var/log /var/lib/nginx /tmp /etc/s6.d
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
