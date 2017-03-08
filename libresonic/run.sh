48225d67f08d7b31ac76b5d505d8727d4180c9b9
Wonderfall <wonderfall@schrodinger.io>
2017-03-08 16:59:44 +0100

    libresonic: fix run.sh

--- libresonic/run.sh
+++ libresonic/run.sh
@@ -8,6 +8,9 @@ chown -R $UID:$GID /data /playlists /libresonic
 
 exec su-exec $UID:$GID tini -- \
 java -Xmx256m \
+  -Dserver.host=0.0.0.0 \
+  -Dserver.port=4040 \
+  -Dserver.contextPath=/ \
   -Dlibresonic.home=/data \
   -Dlibresonic.host=0.0.0.0 \
   -Dlibresonic.port=4040 \
