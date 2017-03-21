462c5a7b42b6c03c6b10f7302ac99d7119c92be2
xataz <xataz@mondedie.fr>
2017-03-21 16:33:03 +0100

    Fix rtorrent-flood

--- rtorrent-flood/run.sh
+++ rtorrent-flood/run.sh
@@ -17,7 +17,7 @@ chown -R $UID:$GID /data /home/torrent /tmp /filebot /usr/flood /flood-db /etc/s
 
 if [ ${RTORRENT_SCGI} -ne 0 ]; then
     sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc 
-    sed -i -e 's|socket: true,|socket: false|' -e 's|port: 5000,|port: '${RTORRENT_SCGI}',|' /usr/flood/config.js
+    sed -i -e 's|socket: true,|socket: false,|' -e 's|port: 5000,|port: '${RTORRENT_SCGI}',|' /usr/flood/config.js
 fi
 
 exec su-exec $GID:$UID /bin/s6-svscan /etc/s6.d
