74befe2cc40ce9a6413cd12dfe535dc6a1d05983
xataz <xataz@users.noreply.github.com>
2017-03-22 16:04:34 +0100

    Add RTORRENT_SCGI variable for use scgi port (#158)
    * Feature(rtorrent-flood) : Add RTORRENT_SCGI variable for use scgi port
    
    * Fix rtorrent-flood

--- rtorrent-flood/run.sh
+++ rtorrent-flood/run.sh
@@ -15,4 +15,9 @@ rm -f /data/.session/rtorrent.lock
 mv /usr/flood /usr/fix && mv /usr/fix /usr/flood # fix strange bug
 chown -R $UID:$GID /data /home/torrent /tmp /filebot /usr/flood /flood-db /etc/s6.d
 
+if [ ${RTORRENT_SCGI} -ne 0 ]; then
+    sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc 
+    sed -i -e 's|socket: true,|socket: false,|' -e 's|port: 5000,|port: '${RTORRENT_SCGI}',|' /usr/flood/config.js
+fi
+
 exec su-exec $GID:$UID /bin/s6-svscan /etc/s6.d
