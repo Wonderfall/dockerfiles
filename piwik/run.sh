be589e5c953e692f144993307d67016ee74575e4
Wonderfall <wonderfall@schrodinger.io>
2017-03-26 01:51:44 +0100

    rename php7.1 packages

--- piwik/run.sh
+++ piwik/run.sh
@@ -6,5 +6,5 @@ fi
 
 ln -s /config/config.ini.php /piwik/config/config.ini.php
 mv piwik fix && mv fix piwik # fix strange bug
-chown -R $UID:$GID /piwik /config /var/log /etc/nginx /etc/php7.1 /var/lib/nginx /tmp /etc/s6.d
+chown -R $UID:$GID /piwik /config /var/log /etc/nginx /etc/php7 /var/lib/nginx /tmp /etc/s6.d
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
