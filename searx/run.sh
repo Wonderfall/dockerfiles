5bffd7bacd4a7511147e545833f49a29b8905bcb
Jan Welker <jan.welker@unibas.ch>
2017-05-19 13:23:07 +0200

    fixed syntax error

--- searx/run.sh
+++ searx/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -i -e "s|base_url : False|base_url : ${BASE_URL}|g" \
+sed -i -e "s/base_url : False/base_url : ${BASE_URL}/g" \
        -e "s/image_proxy : False/image_proxy : ${IMAGE_PROXY}/g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" \
        /usr/local/searx/searx/settings.yml
