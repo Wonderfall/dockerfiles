338e0a290f45080fc36b9f81ae96fda53b8ca76d
Wonderfall <wonderfall@schrodinger.io>
2017-05-27 01:46:23 +0200

    alpine 3.6

---
+++ pgbouncer/README.md
@@ -0,0 +1,10 @@
+## wonderfall/pgbouncer
+
+Minimal image for [PgBouncer](https://pgbouncer.github.io/).
+
+### Volumes
+- /etc/pgbouncer
+
+### Environment variables
+- **GID** : user id *(default : 991)*
+- **UID** : group id *(default : 991)*
--- pgbouncer/run.sh
+++ pgbouncer/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /run/pgbouncer
 chown -R $UID:$GID /etc/pgbouncer /var/log/pgbouncer /run/pgbouncer
-su-exec $UID:$GID pgbouncer /etc/pgbouncer/pgbouncer.ini
+exec su-exec $UID:$GID /sbin/tini -- pgbouncer /etc/pgbouncer/pgbouncer.ini
