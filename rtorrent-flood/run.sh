74934330e172bc08e6efe3165a54d24ba00697c4
Wonderfall <wonderfall@schrodinger.io>
2017-06-04 19:35:55 +0200

    rtorrent-flood: alpine 3.6, fixes, libzen, chromaprint

--- rtorrent-flood/run.sh
+++ rtorrent-flood/run.sh
@@ -8,15 +8,22 @@ mkdir -p /data/Media/TV
 mkdir -p /data/Media/Animes
 mkdir -p /data/Media/Music
 
-sed -i -e "s|<FLOOD_SECRET>|$FLOOD_SECRET|g" /usr/flood/config.js
-sed -i -e "s|<CONTEXT_PATH>|$CONTEXT_PATH|g" /usr/flood/config.js
+sed -i -e "s|<FLOOD_SECRET>|$FLOOD_SECRET|g" \
+       -e "s|<CONTEXT_PATH>|$CONTEXT_PATH|g" /usr/flood/config.js
+
+
+sed -i -e 's#<FILEBOT_RENAME_MOVIES>#'"$FILEBOT_RENAME_MOVIES"'#' \
+       -e 's#<FILEBOT_RENAME_METHOD>#'"$FILEBOT_RENAME_METHOD"'#' \
+       -e 's#<FILEBOT_RENAME_MUSICS>#'"$FILEBOT_RENAME_MUSICS"'#' \
+       -e 's#<FILEBOT_RENAME_SERIES>#'"$FILEBOT_RENAME_SERIES"'#' \
+       -e 's#<FILEBOT_RENAME_ANIMES>#'"$FILEBOT_RENAME_ANIMES"'#' /usr/bin/postdl
 
 rm -f /data/.session/rtorrent.lock
-mv /usr/flood /usr/fix && mv /usr/fix /usr/flood # fix strange bug
+
 chown -R $UID:$GID /data /home/torrent /tmp /filebot /usr/flood /flood-db /etc/s6.d
 
 if [ ${RTORRENT_SCGI} -ne 0 ]; then
-    sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc 
+    sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc
     sed -i -e 's|socket: true,|socket: false,|' -e 's|port: 5000,|port: '${RTORRENT_SCGI}',|' /usr/flood/config.js
 fi
 
