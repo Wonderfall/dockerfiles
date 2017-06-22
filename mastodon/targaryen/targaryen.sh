311dc8cc8adb62a048609cb05c385ab062a5ed43
Wonderfall <wonderfall@schrodinger.io>
2017-06-23 00:11:57 +0200

    mastodon: update targaryen.sh

--- mastodon/targaryen/targaryen.sh
+++ mastodon/targaryen/targaryen.sh
@@ -5,3 +5,7 @@ sed -i -e 's/500/800/g' \
     app/javascript/mastodon/features/compose/components/compose_form.js \
     app/validators/status_length_validator.rb \
     storybook/stories/character_counter.story.js
+
+### Dragon emoji
+sed -i -e 's/1f602/1f432/g' \
+    app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
