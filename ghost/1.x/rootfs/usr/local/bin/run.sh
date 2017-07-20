d4443979d94742f6ec29f18c1cdc38392182d9ba
Wonderfall <wonderfall@schrodinger.io>
2017-07-20 18:23:28 +0200

    ghost: fix config file

--- ghost/1.x/rootfs/usr/local/bin/run.sh
+++ ghost/1.x/rootfs/usr/local/bin/run.sh
@@ -10,13 +10,16 @@ rm -rf /ghost/content/themes/casper &>/dev/null
 cp -r /ghost/casper themes/casper &>/dev/null
 cd /ghost
 
-if [ ! -f /ghost/config.production.json ]; then
+if [ ! -f /ghost/content/ghost.conf ]; then
   echo
   echo "INFO : No configuration file was provided, an example will be used"
   echo "       You can access and modify it in the volume you mounted"
   echo "       Restart in order to apply your changes!"
   echo
   mv /usr/local/etc/ghost.example.conf /ghost/content/ghost.conf
+fi
+
+if [ ! -f /ghost/config.production.json ]; then
   ln -s content/ghost.conf config.production.json
 fi
 
